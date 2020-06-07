c107c0a532664684b6126282fffc6cd5772b4a95
Fernanda Rodrigues da Silva <fernanda.rodrigues.cdc@gmail.com>
2020-06-06 22:31:56 -0700

    Adding route post new todo

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,8 +2,18 @@ const express = require("express");
 const router = express.Router();
 const db = require("../models");
 
+// get all todos
 router.get("/all", (req, res) => {
     db.todo.findAll().then(todos => res.send(todos));
 });
+
+
+// post new todo
+router.post("/new", (req, res) => {
+    db.todo.create({
+        text: req.body.text
+    }).then(submitedtodo => res.send(submitedtodo));
+});
+
 
 module.exports = router;
